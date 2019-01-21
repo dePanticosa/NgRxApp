05d8fd856f194be67d9d791b799538bac7fabc6d
Malinichenko Roman <dePanticosa@gmail.com>
2019-01-21 16:44:36 +0200

    receiving data from store

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Car} from './models/car.model';
+import {Store} from '@ngrx/store';
+import {AppState} from './redux/app.state';
+import {Observable} from 'rxjs/index';
+import {Cars} from "./interfaces/cars.interface";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public cars: Car[] = [];
+  public carsState: Observable<Cars>;
+
+  constructor(private store: Store<AppState>) {}
+
+  ngOnInit() {
+    this.carsState = this.store.select('carPage');
+  }
 
   onAdd(car: Car) {
     this.cars.push(car);
