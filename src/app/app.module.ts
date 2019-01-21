c14cd74a3392d2d87c0efd0deedda362a1c05ebe
Malinichenko Roman <dePanticosa@gmail.com>
2019-01-21 22:20:28 +0200

    routing and devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {RouterModule} from '@angular/router';
 import {StoreModule} from '@ngrx/store';
 import {EffectsModule} from '@ngrx/effects';
-import {HttpClientModule} from '@angular/common/http';
+import {StoreRouterConnectingModule} from '@ngrx/router-store';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
 
 import {AppComponent} from './app.component';
 import {CarsFormComponent} from './components/cars-form/cars-form.component';
@@ -12,6 +15,7 @@ import {CarComponent} from './components/car/car.component';
 import {carsReducer} from './redux/cars.reducer';
 import {CarsService} from './services/cars.service';
 import {CarsEffect} from './redux/cars.effect';
+import {environment} from "../environments/environment";
 
 
 @NgModule({
@@ -25,7 +29,15 @@ import {CarsEffect} from './redux/cars.effect';
     FormsModule,
     StoreModule.forRoot({carPage: carsReducer}),
     HttpClientModule,
-    EffectsModule.forRoot([CarsEffect])
+    EffectsModule.forRoot([CarsEffect]),
+    RouterModule.forRoot(
+      [{
+        path: '',
+        component: AppComponent
+      }]
+    ),
+    StoreRouterConnectingModule,
+    environment.production ? [] : StoreDevtoolsModule.instrument()
   ],
   providers: [CarsService],
   bootstrap: [AppComponent]
