9cb8c5dd68c2f5c2a3f6b14baa1fd3d309f908e0
Malinichenko Roman <dePanticosa@gmail.com>
2019-01-21 16:29:16 +0200

    import StoreModule, add state

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-import { CarsFormComponent } from './components/cars-form/cars-form.component';
-import { CarComponent } from './components/car/car.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {StoreModule} from '@ngrx/store';
+
+import {AppComponent} from './app.component';
+import {CarsFormComponent} from './components/cars-form/cars-form.component';
+import {CarComponent} from './components/car/car.component';
+
+import {carsReducer} from './redux/cars.reducer';
+
 
 @NgModule({
   declarations: [
@@ -14,9 +18,11 @@ import {FormsModule} from '@angular/forms';
   ],
   imports: [
     BrowserModule,
-     FormsModule
+    FormsModule,
+    StoreModule.forRoot({carPage: carsReducer})
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
